26644629a613fd6335945f06fe32219eadbbf9fb
Nitin Birdi <nitin.birdi@icloud.com>
2020-03-14 23:47:44 +0530

    ProjectFinished

--- app.js
+++ app.js
@@ -63,8 +63,66 @@ app.route("/articles")
 // ------>
 
 // ------> request targetting spacific articles
-
+app.route("/articles/:articleTitle")
+  .get(function(req, res) {
+    Article.findOne({
+      title: req.params.articleTitle
+    }, function(err, foundArticle) {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching that title was found.");
+      }
+    });
+  })
+  .put(function(req, res) {
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content,
+      }, {
+        overwrite: true,
+      },
+      function(err, foundArticle) {
+        if (!err) {
+          res.send("Successfully updated the selected article.");
+        }
+      }
+    );
+  })
+  .patch(function(req, res) {
+    Article.update({
+        title: req.params.articleTitle
+      }, {
+        $set: req.body
+      },
+      function(err) {
+        if (!err) {
+          res.send("Successfully updated article.");
+        } else {
+          res.send(err);
+        }
+      });
+  })
+  .delete(function(req, res) {
+    Article.deleteOne({
+        title: req.params.articleTitle
+      },
+      function(err) {
+        if (!err) {
+          res.send("successfully deleted article");
+        } else {
+          res.send("err");
+        }
+      }
+    );
+  });
 // ------>
+
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
